bb499c9db977414fc81918551e19b5a0e8d03c15
Ivan Peresadenko <ivanperesadenkoo@gmail.com>
2024-10-22 14:40:27 +0300

    Configure eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const globals = require("globals");
+
+module.exports = tseslint.config(
+  {
+    "ignores": ['dist/**', 'node_modules/**'],
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    },
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'no-console': ['error'],
+      'no-debugger': ['warn'],
+      'no-undef': ['error'],
+      'prefer-const': ['error'],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'no-trailing-spaces': ['error'],
+      'no-param-reassign': ['error'],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
