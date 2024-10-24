e7848392d21fbf6e721b0f6831678413df2cca73
Ivan Peresadenko <ivanperesadenkoo@gmail.com>
2024-10-24 17:22:50 +0300

    Squares data service

---
+++ src/app/features/squares-game/services/squares-data.service.ts
@@ -0,0 +1,49 @@
+import { WritableSignal } from '@angular/core';
+
+import { EntitiesDataService } from '@core/services/entities-data.service';
+import { SQUARE_COLORS_PALETTE } from '@features/squares-game/constants/square-colors';
+import {
+  CustomColorfulSquare,
+  CustomColorfulSquareArgs,
+} from '@features/squares-game/models/square';
+
+export class SquaresDataService extends EntitiesDataService<
+  CustomColorfulSquare,
+  CustomColorfulSquareArgs
+> {
+  private _squaresNodeList!: NodeList;
+
+  public set squaresNodeList(squaresNodes: NodeList) {
+    this._squaresNodeList = squaresNodes;
+  }
+
+  public get squaresNodeList(): NodeList {
+    return this._squaresNodeList;
+  }
+
+  public getSquareDOMElementByIndex(index: number): Node {
+    return this.squaresNodeList[index];
+  }
+
+  public generateSquares(quantity: number): CustomColorfulSquare[] {
+    return this.generateEntities(
+      CustomColorfulSquare,
+      [SQUARE_COLORS_PALETTE],
+      quantity
+    );
+  }
+
+  public get squares(): CustomColorfulSquare[] {
+    return this.entities;
+  }
+
+  public get squaresSignal(): WritableSignal<CustomColorfulSquare[]> {
+    return this.entitiesSignal;
+  }
+
+  public resetSquaresState(): void {
+    this.squares.forEach((square: CustomColorfulSquare) => {
+      square.setDefaultColor();
+    });
+  }
+}
